2ceffe33910af38609489062fe0bb092d30ec636
John Weis <weis.john@gmail.com>
2015-02-23 07:12:44 -0500

    example against a working submodule

--- apple-example.js
+++ apple-example.js
@@ -2,10 +2,14 @@ var tessel = require('tessel');
 var port = tessel.port['A'];
 var apple = require('tessel-apple-ir-remote')(port);
 
-apple.on('menu.press', function() {
-    console.log('menu.press');
+
+["menu", "center", "up", "down", "right", "left"].forEach(function(b) {
+    apple.on(b, function() {
+        console.log(b, "pressed")
+    })
 });
 
-apple.on('data', function(data) {
-    // you still have access to the low-level data stream
-});
+// apple.on('data', function(data) {
+//     // you still have access to the low-level data stream
+//     console.log(data.toString('hex'));
+// });
