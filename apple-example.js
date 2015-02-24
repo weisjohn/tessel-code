d27ed34435a46d6c57e602e5a0301539669a2c37
John Weis <weis.john@gmail.com>
2015-02-24 02:18:47 -0500

    adding play

--- apple-example.js
+++ apple-example.js
@@ -3,13 +3,16 @@ var port = tessel.port['A'];
 var apple = require('tessel-apple-ir-remote')(port);
 
 
-["menu", "center", "up", "down", "right", "left"].forEach(function(b) {
+["menu", "center", "up", "down", "right", "left", "play"].forEach(function(b) {
     apple.on(b, function() {
         console.log(b, "pressed")
     })
+    apple.on(b + ".long", function() {
+        console.log(b + ".long", "pressed")
+    })
 });
 
-// apple.on('data', function(data) {
-//     // you still have access to the low-level data stream
-//     console.log(data.toString('hex'));
-// });
+apple.on('data', function(data) {
+    // you still have access to the low-level data stream
+    // console.log(data.toString('hex'));
+});
