4cb059901d58de41ab907669ec1cb09554c01afc
John Weis <weis.john@gmail.com>
2015-02-25 23:38:32 -0500

    changes to incorporate both remotes

--- apple-example.js
+++ apple-example.js
@@ -2,17 +2,28 @@ var tessel = require('tessel');
 var port = tessel.port['A'];
 var apple = require('tessel-apple-ir-remote')(port);
 
+function sub_all(prefix) {
+    if (!prefix) prefix = "";
+    ["menu", "center", "up", "down", "right", "left", "play"].forEach(function(b) {
+        b = prefix + b;
+        apple.on(b, function() {
+            console.log(b, "pressed", "\n");
+        });
+        apple.on(b + ".long", function() {
+            console.log(b + ".long", "pressed", "\n");
+        });
+    });
+}
 
-["menu", "center", "up", "down", "right", "left", "play"].forEach(function(b) {
-    apple.on(b, function() {
-        console.log(b, "pressed")
-    })
-    apple.on(b + ".long", function() {
-        console.log(b + ".long", "pressed")
-    })
+// subscribe to all of the id namespaced events
+apple.on('id', function(id) {
+    console.log('id discovered', id);
+    sub_all(id + ".");
 });
 
 apple.on('data', function(data) {
     // you still have access to the low-level data stream
     // console.log(data.toString('hex'));
 });
+
+sub_all();
