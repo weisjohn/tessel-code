bf78ec490d69be3abe39cb738cea7af33e8b1056
John Weis <weis.john@gmail.com>
2015-02-26 20:05:07 -0500

    servo tug of war

--- servo-remote.js
+++ servo-remote.js
@@ -7,13 +7,63 @@ var async = require('async');
 var apple = tar(tessel.port['A']);
 var servo = servolib.use(tessel.port['D']);
 
-// config
-var servo1 = 1; // We have a servo plugged in at position 1
+// config - our servo is plugged into port 1
+var port = 1;
+
+// state - set the servo to the middle
+var position = 0.5;
+
+// first come, first serve
+var first, second;
+
+// helper function for the game
+function move(vector, id) {
+    position += vector * 0.1;
+
+    if (0 >= position || position >= 1) {
+        if (0 >= position) { 
+            console.log(first, "wins");
+        } else {
+            console.log(second, "wins");
+        }
+        throw new Error("game over");
+    }
+    console.log(id, position);
+
+    servo.move(port, position);
+}
 
 // wait for both of the modules to declare they are ready
 async.parallel([
     function(cb) { servo.on('ready', cb) },
     function(cb) { apple.on('ready', cb) },
 ], function() {
-    console.log('ready');
+    
+    // min,max duty cycle for the servo
+    servo.configure(port, 0.05, 0.12, function () {
+        // initialize to middle position
+        servo.move(port, position);    
+    });
+
+    apple.on('id', function(id) {
+
+        // if there are two players, bolt
+        if (second) return;
+
+        // announce entrance in the game
+        console.log('P', !first ? "1" : "2", 'appears: ', id);
+
+        // set the vector for this player
+        var vector = first ? 1 : -1;
+        if (!first) {
+            first = id;
+        } else {
+            second = id;
+        }
+
+        apple.on(id + ".up", function() {
+            move(vector, id);
+        });
+    });
+
 });
