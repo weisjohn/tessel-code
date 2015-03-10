1fbcb97a96e38c377b7c91bcbec2450d4ea4370d
John Weis <weis.john@gmail.com>
2015-03-09 23:03:32 -0400

    switching ports

--- ambient.js
+++ ambient.js
@@ -1,7 +1,7 @@
 var tessel = require('tessel');
 var ambientlib = require('ambient-attx4');
  
-var ambient = ambientlib.use(tessel.port['B']);
+var ambient = ambientlib.use(tessel.port['A']);
  
 ambient.on('ready', function () {
     // get light data
