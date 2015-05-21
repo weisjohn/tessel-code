476bd40cc11cc35a187974cfaeaf6e6dc431aa4c
John Weis <weis.john@gmail.com>
2015-05-21 01:25:43 -0400

    rename to be more clear

--- ir-repeater.js
+++ ir-repeater.js
@@ -5,18 +5,18 @@ infrared.on('ready', function(err) {
     console.log('ready');
 });
 
-var entrant = true;
+var reentrant = true;
 
 infrared.on('data', function(data, other) {
-    if (!entrant) return;
+    if (!reentrant) return;
     console.log('read', data.toString('hex'));
-    entrant = false;
+    reentrant = false;
     setTimeout(function() {
         infrared.sendRawSignal(38, data, function(err) {
             console.log(err ? "failure" : "sent");
             infrared.sendRawSignal(38, data, function(err) {
                 console.log(err ? "failure" : "sent");
-                setTimeout(function() { entrant = true; }, 1e3);
+                setTimeout(function() { reentrant = true; }, 1e3);
             });
         });
     }, 3e3);
