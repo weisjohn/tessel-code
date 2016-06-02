ea4d32dbda64a832522bac7ee5fd98dbe3735561
John Weis <weis.john@gmail.com>
2016-06-02 15:46:17 -0400

    new thermometer which posts values up to a server

---
+++ thermometer.js
@@ -0,0 +1,54 @@
+
+// deps
+var tessel = require('tessel');
+var climatelib = require('climate-si7020');
+var climate = climatelib.use(tessel.port['C']);
+var http = require('http');
+
+// config
+var opts = {
+  method: 'POST',
+  host: '192.168.0.8',
+  port: '8000',
+  path: '/climate',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+};
+var interval = 1e3;
+
+function record(data) {
+
+  // apply some context-based data
+  data.tag = 'sauna';
+  data.date = (new Date()).toString()
+
+  var json = JSON.stringify(data);
+  // console.log(json);
+
+  // create the request and send it
+  var request = http.request(opts, function(res, err) {
+    if (res) res.on('data', function(data) {
+      console.log('response', data.toString());
+    });
+  });
+  request.end(json);
+}
+
+function measure() {
+  climate.readTemperature('f', function (err, temp) {
+    climate.readHumidity(function (err, humidity) {
+      console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humidity.toFixed(4) + '%RH');
+
+      record({ temp: temp, humidity: humidity });
+
+      // read every minute
+      setTimeout(measure, interval);
+    });
+  });
+}
+
+climate.on('ready', function () {
+  console.log('Connected to si7005');
+  measure();
+});
